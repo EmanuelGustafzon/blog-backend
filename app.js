2a82373efa046464e8f2ff5ae14e87ed9b2e5a0c
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2023-06-20 20:13:18 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 const express = require('express')
 const app = express()
-// require('dotenv').config();
 
 app.use(express.json())
 
-// // import mongoose and get key from dotenv
-// const mongoose = require('mongoose');
-// require('dotenv').config()
-// const URL = process.env.mongoDBURL
-
-// // connect to database
-// mongoose.set('strictQuery', true);
-// main().catch(err => console.log(err));
-// async function main() {
-//   await mongoose.connect(URL);
-// }
 
 app.get('/', (req, res) => {
-  res.send('welcome')
+  res.json('welcome')
 })
 
 
