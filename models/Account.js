e55177ebc8ebc3bf451ef6320eade6535ed7bca8
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2023-06-28 20:20:20 +0000

    add account model

---
+++ models/Account.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = Account;
